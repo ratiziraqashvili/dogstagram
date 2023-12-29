c8419b1acc40500afea46d722d713cc0d11ec571
Rati <rziraqashvili@gmail.com>
2023-12-29 12:51:14 +0400

    first commit

---
+++ app/(main)/page.tsx
@@ -0,0 +1,16 @@
+import { getSession } from "next-auth/react"
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const session = await getSession();
+
+  if (!session?.user) {
+    redirect("/login")
+  }
+
+  return (
+      <div className="">
+
+      </div>
+  )
+}
