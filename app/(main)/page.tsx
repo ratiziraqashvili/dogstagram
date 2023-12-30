4dc23e4aa75badd539f80097823caba5e816f3a5
Rati <rziraqashvili@gmail.com>
2023-12-30 16:33:17 +0400

    adding clerk auth

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,13 +1,5 @@
-import { getSession } from "next-auth/react"
-import { redirect } from "next/navigation";
-
 export default async function Home() {
-  const session = await getSession();
-
-  if (!session?.user) {
-    redirect("/register")
-  }
-
+ 
   return (
       <div className="">
 
