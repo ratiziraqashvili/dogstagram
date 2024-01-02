17bf8e64273d3c88b381c61c4285c096d0341f65
Rati <rziraqashvili@gmail.com>
2024-01-02 15:26:19 +0400

    defined prisma for start

--- components/routes.tsx
+++ components/routes.tsx
@@ -1,13 +1,7 @@
-"use client";
-
-import { cn } from "@/lib/utils";
 import { Compass, Home, Search } from "lucide-react";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 
 export const Routes = () => {
-  const pathname = usePathname();
-
   const routes = [
     {
       label: "Home",
@@ -29,22 +23,12 @@ export const Routes = () => {
   return (
     <>
       {routes.map((route) => (
-        <div
-          className={cn(
-            "flex items-center gap-4 p-3 w-full rounded-lg transition hover:bg-primary/10 cursor-pointer duration-300",
-            pathname === route.href && "bg-primary/10"
-          )}
-        >
+        <div className="flex items-center gap-4 p-3 w-full rounded-lg transition hover:bg-primary/5 cursor-pointer duration-300">
           {route.href ? (
             <Link href={route.href}>
               <div className="flex gap-4 items-center">
                 <route.icon className="w-6 h-6" />
-                <span
-                  className={cn(
-                    "hidden xl:block text-md font-[500] pt-[0.20rem]",
-                    pathname === route.href && "font-bold"
-                  )}
-                >
+                <span className="hidden xl:block text-md font-[500] pt-[0.20rem]">
                   {route.label}
                 </span>
               </div>
