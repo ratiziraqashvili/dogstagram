1fd10de4b6911292961640b0692e3894bbcea1bd
Rati <rziraqashvili@gmail.com>
2024-01-14 18:38:07 +0400

    created settings modal

--- components/modals/settings-modal.tsx
+++ components/modals/settings-modal.tsx
@@ -28,18 +28,23 @@ export const SettingsModal = () => {
 
   return (
     <Dialog open={isModalOpen} onOpenChange={handleClose}>
-      <DialogContent className="px-0 pb-0 w-[70%] sm:w-[25rem] gap-0">
-        <div className="flex flex-col h-full mt-3">
+      <DialogContent className="px-0 pb-0 pt-0 w-[70%] sm:w-[25rem] gap-0">
+        <div className="flex flex-col h-full items-center">
           {buttons.map((button) => (
             <button
               key={button.label}
               onClick={button.onClick}
-              className="hover:bg-primary/10 py-3 flex justify-between p-3 text-[0.890rem] transition disabled:pointer-events-none disabled:opacity-50"
+              className="hover:bg-primary/10 w-full py-3 flex justify-center p-3 text-[0.890rem] transition disabled:pointer-events-none disabled:opacity-50 text-center"
             >
-              {button.label}
+              <span>{button.label}</span>
             </button>
           ))}
         </div>
+        <DialogClose>
+          <button className="hover:bg-primary/10 py-3 flex justify-center text-center p-3 text-[0.890rem] transition disabled:pointer-events-none disabled:opacity-50 w-full items-center">
+            <span>Cancel</span>
+          </button>
+        </DialogClose>
       </DialogContent>
     </Dialog>
   );
