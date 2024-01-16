9a6cec41e88e4278726daae0ad306cd4ee329c4c
Rati <rziraqashvili@gmail.com>
2024-01-16 21:57:20 +0400

    handling searching in input and making delete req on onRemove follow

--- components/modals/display-followers-modal.tsx
+++ components/modals/display-followers-modal.tsx
@@ -11,14 +11,19 @@ import {
 import { useModal } from "@/hooks/use-modal-store";
 import axios from "axios";
 import { X } from "lucide-react";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import SkeletonItem from "../skeleton-item";
 import { ProfilePicture } from "../profile-picture";
-import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { Button } from "../ui/button";
+import { useAuth } from "@clerk/nextjs";
+import { useFollowingStore } from "@/hooks/use-following-store";
 
 export const DisplayFollowersModal = () => {
+  const { userId } = useAuth();
+  const { setIsFollowing, setFollowerCount, followerCount } =
+  useFollowingStore();
   const [followers, setFollowers] = useState<
     {
       follower: {
@@ -29,15 +34,17 @@ export const DisplayFollowersModal = () => {
       };
     }[]
   >([]);
+  const [search, setSearch] = useState("");
   const [isLoading, setIsLoading] = useState(true);
   const { isOpen, onClose, type } = useModal();
-  const userId = usePathname().split("/")[1];
+  const otherUserId = usePathname().split("/")[1];
+  const router = useRouter();
 
   useEffect(() => {
     // Fetch following data
     const fetchFollowers = async () => {
       try {
-        const response = await axios.get(`/api/${userId}/followerdata`);
+        const response = await axios.get(`/api/${otherUserId}/followerdata`);
         setFollowers(response.data);
       } catch (error) {
         console.error("Error fetching following data:", error);
@@ -47,13 +54,32 @@ export const DisplayFollowersModal = () => {
     };
 
     fetchFollowers();
-  }, [userId]);
+  }, [otherUserId]);
 
   const isModalOpen = isOpen && type === "displayFollowers";
 
   const handleClose = () => {
     onClose();
   };
+
+  const onRemoveFollow = async () => {
+    setIsLoading(true);
+    try {
+      await axios.delete(`/api/users/unfollow/${userId}`);
+
+      setIsFollowing(false);
+      router.refresh();
+      setFollowerCount(followerCount - 1);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  const onFollow = async () => {
+    
+  }
 
   return (
     <CommandDialog open={isModalOpen} onOpenChange={handleClose}>
@@ -64,12 +90,23 @@ export const DisplayFollowersModal = () => {
           className="w-5 h-5 absolute right-4 top-2.5 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground outline-none cursor-pointer"
         />
       </div>
-      <CommandInput placeholder="Search" />
+      <CommandInput
+        onChangeCapture={(e) => {
+          const input = e.target as HTMLInputElement;
+          setSearch(input.value);
+        }}
+        placeholder="Search"
+      />
       <CommandList>
         <CommandEmpty>No results found.</CommandEmpty>
         <CommandGroup heading="">
           <CommandItem>
-            {isLoading ? (
+            {followers.length === 0 && !isLoading && (
+              <div className="pl-[35.5%] whitespace-nowrap flex justify-center">
+                <p className="text-muted-foreground">No result found.</p>
+              </div>
+            )}
+            {isLoading && (
               <div className="flex flex-col gap-4">
                 {Array(4)
                   .fill(undefined)
@@ -77,37 +114,51 @@ export const DisplayFollowersModal = () => {
                     <SkeletonItem key={index} />
                   ))}
               </div>
-            ) : (
-              <div
-                className={cn(
-                  "w-full",
-                  followers.length > 5 && "overflow-y-scroll"
-                )}
-              >
-                {followers.map((follower) => (
-                  <div className="pb-4" key={follower.follower.username}>
-                    <div className="flex items-center space-x-4">
-                      <Link onClick={handleClose} href={`/${follower.follower.clerkId}`}>
-                        <ProfilePicture
-                          className="h-10 w-10 cursor-pointer"
-                          imageUrl={follower.follower.imageUrl}
-                        />
-                      </Link>
-                      <Link onClick={handleClose} href={`/${follower.follower.clerkId}`}>
-                        <div className="cursor-pointer">
-                          <h2 className="font-semibold">
-                            {follower.follower.username}
-                          </h2>
-                          <h2 className="text-muted-foreground">
-                            {follower.follower.firstName}
-                          </h2>
-                        </div>
-                      </Link>
-                    </div>
-                  </div>
-                ))}
-              </div>
             )}
+            <div className="w-full">
+                {followers
+                  .filter((follower) => follower.follower.username.includes(search))
+                  .map((follower) => (
+                    <div
+                      className="flex justify-between pb-4"
+                      key={follower.follower.username}
+                    >
+                      <div className="flex items-center space-x-4">
+                        <Link
+                          onClick={handleClose}
+                          href={`/${follower.follower.clerkId}`}
+                        >
+                          <ProfilePicture
+                            className="h-11 w-11 cursor-pointer"
+                            imageUrl={follower.follower.imageUrl}
+                          />
+                        </Link>
+                        <Link
+                          onClick={handleClose}
+                          href={`/${follower.follower.clerkId}`}
+                        >
+                          <div className="cursor-pointer">
+                            <h2 className="font-semibold">
+                              {follower.follower.username}
+                            </h2>
+                            <h2 className="text-muted-foreground">
+                              {follower.follower.firstName}
+                            </h2>
+                          </div>
+                        </Link>
+                      </div>
+                      <div>
+                        <Button
+                          onClick={otherUserId === userId ? onRemoveFollow : onFollow}
+                          variant={otherUserId === userId ? "default" : "amber"}
+                          className="h-[2rem] w-[5rem]"
+                        >
+                          {otherUserId === userId ? "Remove" : "Follow"}
+                        </Button>
+                      </div>
+                    </div>
+                  ))}
+            </div>
           </CommandItem>
         </CommandGroup>
       </CommandList>
