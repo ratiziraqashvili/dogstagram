73929fc49a604f0c48c841d790a1cb2647822315
Rati <rziraqashvili@gmail.com>
2024-01-29 16:57:38 +0400

    storing cloudinary data in local storage for better development

--- components/modals/create-post.tsx
+++ components/modals/create-post.tsx
@@ -8,8 +8,10 @@ import Image from "next/image";
 export const CreatePostModal = () => {
   const { isOpen, onClose, type } = useModal();
   const { uploadedData } = usePostDataStore();
+  const storedData = localStorage.getItem("uploadedData");
+  const data = storedData ? JSON.parse(storedData) : uploadedData;
 
-  console.log(uploadedData)
+  console.log(data);
 
   const isModalOpen = isOpen && type === "createPost";
 
@@ -23,11 +25,13 @@ export const CreatePostModal = () => {
         <DialogClose>
           <X className="w-5 h-5 absolute right-4 top-2.5 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground outline-none cursor-pointer" />
         </DialogClose>
-          <Image
-           src={uploadedData?.info?.secure_url || null}
-           fill
-           alt="Image"
-          />
+        <Image
+          src={uploadedData?.info?.secure_url}
+          alt="Image"
+          height={200}
+          width={200}
+        />
+        dasd
       </DialogContent>
     </Dialog>
   );
--- components/routes.tsx
+++ components/routes.tsx
@@ -19,6 +19,7 @@ export const Routes = () => {
     widget.close();
 
     addUploadedData(result);
+    localStorage.setItem('uploadedData', JSON.stringify(result));
 
     onCreatePostModalOpen();
   };
