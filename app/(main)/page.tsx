250647666d436be3f6f3f9bd9edea4aeed42fc30
Rati <rziraqashvili@gmail.com>
2024-02-20 15:22:16 +0400

    getting mutual posts

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,11 +1,58 @@
 import { MainPosts } from "@/components/main-posts";
 import { ProfileIndicator } from "@/components/profile-indicator";
+import { db } from "@/lib/db";
+import { currentUser } from "@clerk/nextjs";
 
-export default  function Home() {
+export default async function Home() {
+  const user = await currentUser();
+
+  const currUserRelatives = await db.user.findFirst({
+    where: {
+      clerkId: user?.id,
+    },
+    select: {
+      following: true,
+      followers: true,
+    },
+  });
+
+  const followingIds = currUserRelatives?.following.map(
+    (user) => user.followingId
+  );
+  const followerIds = currUserRelatives?.followers.map(
+    (user) => user.followerId
+  );
+
+  const ids = [...(followingIds ?? []), ...(followerIds ?? [])];
+
+  const posts = await db.post.findMany({
+    where: {
+      userId: {
+        in: ids,
+      },
+    },
+    include: {
+      _count: {
+        select: {
+          likes: true,
+          comments: true,
+        },
+      },
+      user: {
+        select: {
+          imageUrl: true,
+          username: true,
+        },
+      },
+    },
+  });
+
+  console.log(posts)
+
   return (
     <div className="">
       <div className="flex">
-        <MainPosts />
+        <MainPosts posts={posts} />
         <ProfileIndicator />
       </div>
     </div>
--- components/main-posts.tsx
+++ components/main-posts.tsx
@@ -1,7 +1,13 @@
-export const MainPosts = () => {
+import { PostInfoType } from "@/types"
+
+interface MainPostsProps {
+    posts: PostInfoType;
+}
+
+export const MainPosts = ({ posts }: MainPostsProps) => {
     return (
-        <div className="bg-green-500 flex-1 mx-auto lg:max-w-[60%] md:max-w-[80%] max-w-full xl:ml-[20rem] md:mt-0 mt-[3.8rem]">
-            mainile
+        <div className="flex-1 mx-auto lg:max-w-[60%] md:max-w-[80%] max-w-full xl:ml-[20rem] md:mt-0 mt-[3.8rem]">
+            <div></div>
         </div>
     )
 }
